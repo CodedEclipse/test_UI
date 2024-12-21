2f76878cc4b7e3c7c73d9aa0a1f318bcff0d14ba
Manish Deotale <manishdeotalemr@gmail.com>
2024-12-21 11:38:09 +0530

    chnage

--- src/home/componants/Registration.js
+++ src/home/componants/Registration.js
@@ -8,10 +8,7 @@ import { set_admin_logged, admin_logged_data } from '../../Configuration';
 
 function Registration() {
     const navigate = useNavigate();
-    const [yData, setYData] = useState('');
-    useEffect(() => {
-
-    }, []);
+    const [passwordVisible, setPasswordVisible] = useState(false); // State to toggle password visibility
 
     const registerData = useFormik({
         initialValues: {
@@ -23,37 +20,40 @@ function Registration() {
         },
         validationSchema: RegistrationSchema,
         onSubmit: async (values) => {
-            values.username = values.email_id
+            values.username = values.email_id;
             await _post_data('/cust/CustomerRegistration', values)
                 .then((res) => {
-                    console.log('res', res.data);
-                    res.data = JSON.parse(res.data)
+                    res.data = JSON.parse(res.data);
                     if (res.data.status) {
                         registerData.resetForm();
                         navigate("/");
+                        Toast(1, res.data.message);
                     } else {
                         Toast(2, res.data.message);
                     }
                 }).catch((e) => {
-                    console.log('e', e);
                     Toast(2, e.response.data.error);
                 });
         },
     });
 
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
 
     return (
         <>
             <section className="wrapper">
                 <div className="container">
-                    <form onSubmit={registerData.handleSubmit}>
+                    <form onSubmit={registerData.handleSubmit} autoComplete="off">
                         <div className="row m-3">
                             <div className="col">
-                                <label for="inputState" className="m-2">First Name</label>
-                                <input type="text" className="form-control"  name="firstname"
+                                <label htmlFor="inputState" className="m-2">First Name</label>
+                                <input type="text" className="form-control" name="firstname"
                                     value={registerData.values.firstname}
                                     onChange={registerData.handleChange}
                                     onBlur={registerData.handleBlur}
+                                    autoComplete="off"
                                 />
                                 {registerData.touched.firstname && registerData.errors.firstname && (
                                     <span className="login-danger validation-text">* &nbsp;
@@ -62,11 +62,12 @@ function Registration() {
                                 )}
                             </div>
                             <div className="col">
-                                <label for="inputState" className="m-2">Last Name</label>
-                                <input type="text" className="form-control"  name="lastname"
+                                <label htmlFor="inputState" className="m-2">Last Name</label>
+                                <input type="text" className="form-control" name="lastname"
                                     value={registerData.values.lastname}
                                     onChange={registerData.handleChange}
                                     onBlur={registerData.handleBlur}
+                                    autoComplete="off"
                                 />
                                 {registerData.touched.lastname && registerData.errors.lastname && (
                                     <span className="login-danger validation-text">* &nbsp;
@@ -77,11 +78,12 @@ function Registration() {
                         </div>
                         <div className="row m-3">
                             <div className="col">
-                                <label for="inputState" className="m-2">Mobile NO</label>
+                                <label htmlFor="inputState" className="m-2">Mobile NO</label>
                                 <input type="text" className="form-control" maxLength={10} name="mobile_no"
                                     value={registerData.values.mobile_no}
                                     onChange={registerData.handleChange}
                                     onBlur={registerData.handleBlur}
+                                    autoComplete="off"
                                 />
                                 {registerData.touched.mobile_no && registerData.errors.mobile_no && (
                                     <span className="login-danger validation-text">* &nbsp;
@@ -90,11 +92,12 @@ function Registration() {
                                 )}
                             </div>
                             <div className="col">
-                                <label for="inputState" className="m-2">E-Mail ID</label>
+                                <label htmlFor="inputState" className="m-2">E-Mail ID</label>
                                 <input type="email" className="form-control" name="email_id"
                                     value={registerData.values.email_id}
                                     onChange={registerData.handleChange}
                                     onBlur={registerData.handleBlur}
+                                    autoComplete="off"
                                 />
                                 {registerData.touched.email_id && registerData.errors.email_id && (
                                     <span className="login-danger validation-text">* &nbsp;
@@ -105,12 +108,13 @@ function Registration() {
                         </div>
                         <div className="row m-3">
                             <div className="col">
-                                <label for="inputState" className="m-2">User Name</label>
-                                <input type="text" className="form-control"  name="email_id"
+                                <label htmlFor="inputState" className="m-2">User Name</label>
+                                <input type="text" className="form-control" name="email_id"
                                     value={registerData.values.email_id}
                                     onChange={registerData.handleChange}
                                     onBlur={registerData.handleBlur}
                                     disabled
+                                    autoComplete="off"
                                 />
                                 {registerData.touched.email_id && registerData.errors.email_id && (
                                     <span className="login-danger validation-text">* &nbsp;
@@ -119,12 +123,29 @@ function Registration() {
                                 )}
                             </div>
                             <div className="col">
-                                <label for="inputState" className="m-2">Password</label>
-                                <input type="password" className="form-control"  name="password"
-                                    value={registerData.values.password}
-                                    onChange={registerData.handleChange}
-                                    onBlur={registerData.handleBlur}
-                                />
+                                <label htmlFor="inputState" className="m-2">Password</label>
+                                <div className="password-wrapper position-relative">
+                                    <input 
+                                        type={passwordVisible ? "text" : "password"} 
+                                        className="form-control" 
+                                        name="password"
+                                        value={registerData.values.password}
+                                        onChange={registerData.handleChange}
+                                        onBlur={registerData.handleBlur}
+                                        autoComplete="new-password"
+                                    />
+                                    <i 
+                                        onClick={togglePasswordVisibility} 
+                                        className={`fa ${passwordVisible ? 'fa-eye' : 'fa-eye-slash'}`}
+                                        style={{
+                                            position: 'absolute',
+                                            right: '10px',
+                                            top: '50%',
+                                            transform: 'translateY(-50%)',
+                                            cursor: 'pointer',
+                                        }}
+                                    ></i>
+                                </div>
                                 {registerData.touched.password && registerData.errors.password && (
                                     <span className="login-danger validation-text">* &nbsp;
                                         {registerData.errors.password}
@@ -134,17 +155,16 @@ function Registration() {
                         </div>
                         <div className="row m-4">
                             <button type="submit" className="btn btn-outline" style={{ width: '172px' }} >Register</button>
-                            <Link to='/'  className="btn btn-outline ml-5" style={{ width: '172px',marginLeft: '40px' }}>
+                            <Link to='/' className="btn btn-outline ml-5" style={{ width: '172px', marginLeft: '40px' }}>
                                 Cancel
                             </Link>
                         </div>
                     </form>
                 </div>
-
             </section>
-
         </>
     );
 }
+
 
 export default Registration;
